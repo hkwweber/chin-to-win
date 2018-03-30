c5b5a55ccd947f10e5ccb57a5533b84f4cf6683d
Hannah Weber <hkwweber@gmail.com>
2018-03-30 15:09:00 -0400

    fix package.json

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ const PORT = 3000;
 
 db.sync({})
 .then(() => {
-  console.log('db synced great job')
+  console.log('db synced wooowwww')
   app.listen(PORT, () => console.log(`listening to you on ${PORT}`))
 });
